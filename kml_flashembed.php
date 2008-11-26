2abd2f107260c8dbea71fb27a22614ccd8db890e
Kimili <Kimili@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-26 22:02:50 +0000

    Stole shortcode parsing to work tag.
    git-svn-id: https://plugins.svn.wordpress.org/kimili-flash-embed/branches/2.0@76067 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kml_flashembed.php
+++ kml_flashembed.php
@@ -32,7 +32,7 @@ class KimiliFlashEmbed
 		// Register Hooks
 		if (is_admin()) {
 			
-			// Register editor button hooks
+			// Register editor buttons
 			add_filter( 'tiny_mce_version', array(&$this, 'tiny_mce_version') );
 			add_filter( 'mce_external_plugins', array(&$this, 'mce_external_plugins') );
 			add_action( 'edit_form_advanced', array(&$this, 'add_quicktags') );
@@ -52,9 +52,72 @@ class KimiliFlashEmbed
 		wp_enqueue_script( 'swfobject', plugins_url('/kimili-flash-embed/js/swfobject.js'), array(), '2.1' );
 	}
 	
+	public function parseShortcodes($content)
+	{
+		//$pattern = '/(<p>[\s\n\r]*)?\[(kml_(flash|swf)embed)\b(.*?)(?:(\/))?\](?:(.+?)\[\/\2\])?([\s\n\r]*<\/p>)?/Umi';
+		$pattern = '/(<p>[\s\n\r]*)?\[(kml_(flash|swf)embed)\b(.*?)(?:(\/))?\](?:(.+?)\[\/\2\])?([\s\n\r]*<\/p>)?/s';
+		$result = preg_replace_callback($pattern, array(&$this, 'processShortcode'), $content);
+		return $result;
+	}
+	
+	public function processShortcode($code)
+	{
+		$r	= "";
+
+		$atts = $this->parseAtts($code[4]);
+		$altContent = $code[6];
+
+		$attpairs	= preg_split('/\|/', $elements, -1, PREG_SPLIT_NO_EMPTY);
+
+		if (isset($atts['movie']) && isset($atts['height']) && isset($atts['width'])) {
+
+			$atts['fversion']	= (isset($atts['fversion'])) ? $atts['fversion'] : 6;
+
+			if (isset($atts['fvars'])) {
+				$fvarpair_regex		= "/(?<!([$|\?]\{))\s+;\s+(?!\})/";
+				$atts['fvars']		= preg_split($fvarpair_regex, $atts['fvars'], -1, PREG_SPLIT_NO_EMPTY);
+			}
+
+			// Convert any quasi-HTML in alttext back into tags
+			$atts['alttext']		= (isset($atts['alttext'])) ? preg_replace("/{(.*?)}/i", "<$1>", $atts['alttext']) : '';
+
+			// If we're not serving up a feed, generate the script tags
+			//if (is_feed()) {
+			//	$r	= kml_flashembed_build_fo_script($atts);
+			//} else {
+			//	$r	= kml_flashembed_build_object_tag($atts);
+			//}
+		}
+	 	return 'KFE: '. join('<br />', $atts);
+	}
+	
+	public function parseAtts($text)
+	{
+		$atts = array();
+		$pattern = '/(\w+)\s*=\s*"([^"]*)"(?:\s|$)|(\w+)\s*=\s*\'([^\']*)\'(?:\s|$)|(\w+)\s*=\s*([^\s\'"]+)(?:\s|$)|"([^"]*)"(?:\s|$)|(\S+)(?:\s|$)/';
+		$text = preg_replace("/[\x{00a0}\x{200b}]+/u", " ", $text);
+		if ( preg_match_all($pattern, $text, $match, PREG_SET_ORDER) ) {
+			foreach ($match as $m) {
+				if (!empty($m[1]))
+					$atts[strtolower($m[1])] = stripcslashes($m[2]);
+				elseif (!empty($m[3]))
+					$atts[strtolower($m[3])] = stripcslashes($m[4]);
+				elseif (!empty($m[5]))
+					$atts[strtolower($m[5])] = stripcslashes($m[6]);
+				elseif (isset($m[7]) and strlen($m[7]))
+					$atts[] = stripcslashes($m[7]);
+				elseif (isset($m[8]))
+					$atts[] = stripcslashes($m[8]);
+			}
+		} else {
+			$atts = ltrim($text);
+		}
+		return $atts;
+	}
+	
 	public function doObStart()
 	{
-		# code...
+		ob_start(array(&$this, 'parseShortcodes'));
 	}
 	
 	// Break the browser cache of TinyMCE
@@ -77,6 +140,7 @@ class KimiliFlashEmbed
 		return $buttons;
 	}
 	
+	// Add a button to the quicktag view
 	function add_quicktags()
 	{
 		$buttonshtml = '<input type="button" class="ed_button" onclick="Kimili.Flash.embed(); return false;" title="Embed a Flash Movie in your post" value="Kimili Flash Embed" />';
