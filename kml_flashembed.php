0e0e8fbb60cd2666eb025227a070b11d144f3860
Kimili <Kimili@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-26 22:22:45 +0000

    Doing twice-baked object tags
    git-svn-id: https://plugins.svn.wordpress.org/kimili-flash-embed/branches/2.0@76070 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kml_flashembed.php
+++ kml_flashembed.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Kimili Flash Embed
 Plugin URI: http://www.kimili.com/plugins/kml_flashembed
-Description: Provides a full Wordpress interface for <a href="http://code.google.com/p/swfobject/">SWFObject</a> - the best way to embed Flash on a site.
+Description: Provides a full Wordpress interface for <a href="http://code.google.com/p/swfobject/">SWFObject</a> - the best way to embed Flash on your site.
 Version: 2.0
 Author: Michael Bester
 Author URI: http://www.kimili.com
@@ -54,18 +54,18 @@ class KimiliFlashEmbed
 	
 	public function parseShortcodes($content)
 	{
-		//$pattern = '/(<p>[\s\n\r]*)?\[(kml_(flash|swf)embed)\b(.*?)(?:(\/))?\](?:(.+?)\[\/\2\])?([\s\n\r]*<\/p>)?/Umi';
 		$pattern = '/(<p>[\s\n\r]*)?\[(kml_(flash|swf)embed)\b(.*?)(?:(\/))?\](?:(.+?)\[\/\2\])?([\s\n\r]*<\/p>)?/s';
 		$result = preg_replace_callback($pattern, array(&$this, 'processShortcode'), $content);
 		return $result;
 	}
 	
+	// Thanks to WP shortcode API Code
 	public function processShortcode($code)
 	{
 		$r	= "";
 
 		$atts = $this->parseAtts($code[4]);
-		$altContent = $code[6];
+		$altContent = isset($code[6]) ? $code[6] : '';
 
 		$attpairs	= preg_split('/\|/', $elements, -1, PREG_SPLIT_NO_EMPTY);
 
@@ -79,18 +79,19 @@ class KimiliFlashEmbed
 			}
 
 			// Convert any quasi-HTML in alttext back into tags
-			$atts['alttext']		= (isset($atts['alttext'])) ? preg_replace("/{(.*?)}/i", "<$1>", $atts['alttext']) : '';
+			$atts['alttext']		= (isset($atts['alttext'])) ? preg_replace("/{(.*?)}/i", "<$1>", $atts['alttext']) : $altContent;
 
 			// If we're not serving up a feed, generate the script tags
-			//if (is_feed()) {
-			//	$r	= kml_flashembed_build_fo_script($atts);
-			//} else {
-			//	$r	= kml_flashembed_build_object_tag($atts);
-			//}
+			if (is_feed()) {
+				$r	= $this->buildObjectTag($atts);
+			} else {
+				$r	= $this->buildObjectTag($atts);
+			}
 		}
-	 	return 'KFE: '. join('<br />', $atts);
+	 	return $r;
 	}
 	
+	// Thanks to WP shortcode API Code
 	public function parseAtts($text)
 	{
 		$atts = array();
@@ -113,6 +114,76 @@ class KimiliFlashEmbed
 			$atts = ltrim($text);
 		}
 		return $atts;
+	}
+	
+	public function buildObjectTag($atts)
+	{
+		$out	= array();	
+		if (is_array($atts)) extract($atts);
+
+		// Build a query string based on the $fvars attribute
+		$querystring = (count($fvars) > 0) ? "?" : "";
+		for ($i = 0; $i < count($fvars); $i++) {
+			$thispair	= trim($fvars[$i]);
+			$nvpair		= explode("=",$thispair);
+			$name		= trim($nvpair[0]);
+			$value		= "";
+			for ($j = 1; $j < count($nvpair); $j++) {			// In case someone passes in a fvars with additional "="
+				$value		.= trim($nvpair[$j]);
+				$value		= preg_replace('/&#038;/', '&', $value);
+				if ((count($nvpair) - 1)  != $j) {
+					$value	.= "=";
+				}
+			}
+			// Prune out JS or PHP values
+			if (preg_match("/^\\$\\{.*\\}/i", $value)) { 		// JS
+				$endtrim 	= strlen($value) - 3;
+				$value		= substr($value, 2, $endtrim);
+				$value		= str_replace(';', '', $value);
+			} else if (preg_match("/^\\?\\{.*\\}/i", $value)) {	// PHP
+				$endtrim 	= strlen($value) - 3;
+				$value 		= substr($value, 2, $endtrim);
+				$value 		= eval("return " . $value);
+			}
+			// else {
+			//	$value = '"'.$value.'"';
+			//}
+			$querystring .= $name . '=' . $value;
+			if ($i < count($fvars) - 1) {
+				$querystring .= "&";
+			}
+		}
+
+										$out[] = '';    
+										$out[] = '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"';
+										$out[] = '			width="'.$width.'"';
+										$out[] = '			height="'.$height.'">';										
+										$out[] = '	<param name="movie" value="' . $movie.$querystring . '" />';
+		if (isset($play))				$out[] = '	<param name="play" value="' . $play . '" />';
+		if (isset($loop))				$out[] = '	<param name="loop" value="' . $loop . '" />';
+		if (isset($menu)) 				$out[] = '	<param name="menu" value="' . $menu . '" />';
+		if (isset($scale)) 				$out[] = '	<param name="scale" value="' . $scale . '" />';
+		if (isset($wmode)) 				$out[] = '	<param name="wmode" value="' . $wmode . '" />';
+		if (isset($align)) 				$out[] = '	<param name="align" value="' . $align . '" />';
+		if (isset($salign)) 			$out[] = '	<param name="salign" value="' . $salign . '" />';    
+		if (isset($base)) 	   		 	$out[] = '	<param name="base" value="' . $base . '" />';
+		if (isset($allowscriptaccess))	$out[] = '	<param name="allowScriptAccess" value="' . $allowscriptaccess . '" />';
+		if (isset($allowfullscreen))	$out[] = '	<param name="allowFullScreen" value="' . $allowfullscreen . '" />';
+										$out[] = '	<!--[if !IE]>-->';
+							  	  		$out[] = '	<object	type="application/x-shockwave-flash"';
+										$out[] = '			data="'.$movie.$querystring.'"'; 
+		if (isset($base)) 	   		 	$out[] = '			base="'.$base.'"';
+										$out[] = '			width="'.$width.'"';
+										$out[] = '			height="'.$height.'">';
+										$out[] = '	<!--[endif]>-->';
+		if (isset($alttext))			$out[] = '		'.$alttext;
+										$out[] = '	<!--[if !IE]>-->';
+							  	  		$out[] = '	</object>';
+										$out[] = '	<!--[endif]>-->';
+		 								$out[] = '</object>';     
+
+		$ret .= join("\n", $out);
+		return $ret;
 	}
 	
 	public function doObStart()
